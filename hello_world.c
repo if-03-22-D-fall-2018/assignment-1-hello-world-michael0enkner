12c603b9f4a88b29c8c77bcb5ceaef84cb51d52a
michael0enkner <fmgalaxy.office@gmail.com>
2018-09-20 09:44:11 +0200

    First Programm

--- hello_world.c
+++ hello_world.c
@@ -1,16 +1,12 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class>
+ *				HTBLA-Leonding / Class: <2Dhif>
  * ---------------------------------------------------------
  * Exercise Number: 0
- * Title:			<title of the assignment>
- * Author:			<your name>
+ * Title:			<Hello Michael Enkner>
+ * Author:			<Michael Enkner>
  * ----------------------------------------------------------
  * Description:
- * <some brief description, what the program does: example:
- * Calculates a pyramid of numbers, i.e., it multiplies a big
- * integer by the number 2, its result by 3, etc. and then
- * returns to the original value by subsequently dividing,
- * again starting by 2, 3, etc.>
+ * <prints out Hello Michael Enkner>
  * ----------------------------------------------------------
  */
 #include <stdio.h>
